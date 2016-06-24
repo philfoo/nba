30d546649e5ba0df3f842ab46aad88379cc7e3af
Phil <philphoo95@gmail.com>
2016-06-23 23:15:38 -0700

    spaces?

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const request = require('request');
 
 request.get({
-	url: 'https://erikberg.com/nba/boxscore/20120621-oklahoma-city-thunder-at-miami-heat.json',
-	headers: {
-		'Authorization': 'Bearer ' + process.env.TOKEN,
-		'User-Agent': 'philip.foo@duke.edu'
-	}
+    url: 'https://erikberg.com/nba/boxscore/20120621-oklahoma-city-thunder-at-miami-heat.json',
+    headers: {
+        'Authorization': 'Bearer ' + process.env.TOKEN,
+        'User-Agent': 'philip.foo@duke.edu'
+    }
 }, function(error, response, body){
-	if(error){
-		console.log(error);
-	}else{
-		console.log(body);
-	}
+    if(error){
+        console.log(error);
+    }else{
+        console.log(body);
+    }
 });
