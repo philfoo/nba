5e28a6a9c25174714781347afb5b7c6b2ec9cc57
Phil <philphoo95@gmail.com>
2016-06-25 02:31:31 -0700

    callback fun

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 const request = require('request');
+const async = require('async');
+const utils = require('./utils').utils;
 const MongoClient = require('mongodb').MongoClient;
+
+
+var currentDate = new Date('December 16, 2011 00:00:00');
+var endingDate = new Date('June 24, 2016 00:00:00');
+
+//Go through all dates, grab events array
+//Iterate through events array, grabbing event_id
+//Query every single event ID
+var datesArray = [];
+while(currentDate.getTime() < endingDate.getTime()){
+    datesArray.push(utils.formatDate(currentDate));
+    currentDate = utils.getNextDate(currentDate);
+};
+
+
+var i = 0;
+var eventIdArray = [];
+var myInterval = setInterval(function(){
+    if (i >= datesArray.length){
+        clearInterval(myInterval)
+    }
+    utils.queryXMLEvents(datesArray[i], function(dayArray){
+        for(var j = 0; j < dayArray.length; j++){
+            eventIdArray.push(dayArray[j]);
+        }   
+        console.log(eventIdArray);
+    });
+    i++;
+}, 11*1000);
+
 
 
 var mongo_url = 'mongodb://' + process.env.MONGO_USER + ":" + process.env.MONGO_PASSWORD + "@ds023054.mlab.com:23054/heroku_g83bgn25";
@@ -15,17 +47,7 @@ MongoClient.connect(mongo_url, function(err, db){
     });
 });
 
-request.get({
-    url: 'https://erikberg.com/nba/boxscore/20120621-oklahoma-city-thunder-at-miami-heat.json',
-    headers: {
-        'Authorization': 'Bearer ' + process.env.TOKEN,
-        'User-Agent': 'philip.foo@duke.edu'
-    }
-}, function(error, response, body){
-    if(error){
-        console.log(error);
-    }else{
-        console.log(JSON.parse(body));
-    }
-});
+
+
+
 
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,53 @@
+const request = require('request');
+
 var utils = {
 	getNextDate: function(date){
+		date.setDate(date.getDate()+1);
+		return date;
+	},
+
+	formatDate: function(date){
+		var dd = date.getDate().toString();
+		var mm = (date.getMonth()+1).toString();
+		var yyyy = (date.getFullYear()).toString();
+		if (mm.length == 1){
+			mm = '0' + mm;
+		}
+		if (dd.length == 1){
+			dd = '0' + dd;
+		}
+		return yyyy + mm + dd;
+	},
+
+	queryXMLEvents: function(date, callback){
+		var eventsUrl = 'https://erikberg.com/events.json?date=' + date + '&sport=nba';
+		var eventIds = [];
+		request.get({
+		    url: eventsUrl,
+		    headers: {
+		        'Authorization': 'Bearer ' + process.env.TOKEN,
+		        'User-Agent': 'philip.foo@duke.edu'
+		    }
+		}, function(error, response, body){
+		    if(error){
+		        console.log(error);
+		    }
+		    else{
+		        var eventsArray = JSON.parse(body).event;
+		        for (var i = 0; i < eventsArray.length; i++){
+		        	eventIds.push(eventsArray[i].event_id);
+		        }
+		        callback(eventIds);
+		    }
+		});
+	},
+
+	queryGameStats: function(date){
 
 	},
 
-	queryXmlStats: function(date){
+	postToMongo: function(obj){
 
-	},
-
-	postToMongo: function(date){
-		
 	}
 };
 
