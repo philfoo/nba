411f5c90056395d3602f65d8f64c02598ced56a5
Phil <philphoo95@gmail.com>
2016-06-24 23:57:34 -0700

    working connection to mongodb

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const request = require('request');
+const MongoClient = require('mongodb').MongoClient;
+
+
+var mongo_url = 'mongodb://' + process.env.MONGO_USER + ":" + process.env.MONGO_PASSWORD + "@ds023054.mlab.com:23054/heroku_g83bgn25";
+MongoClient.connect(mongo_url, function(err, db){
+    var collection = db.collection('nba_games');
+    collection.insertOne({a: 1}, function(err, r){
+        if(err){
+            console.log("There was an error");
+        }else{
+            console.log("Success!");
+            db.close();
+        }
+    });
+});
 
 request.get({
     url: 'https://erikberg.com/nba/boxscore/20120621-oklahoma-city-thunder-at-miami-heat.json',
@@ -10,6 +25,7 @@ request.get({
     if(error){
         console.log(error);
     }else{
-        console.log(body);
+        console.log(JSON.parse(body));
     }
 });
+
---
+++ src/utils.js
@@ -0,0 +1,15 @@
+var utils = {
+	getNextDate: function(date){
+
+	},
+
+	queryXmlStats: function(date){
+
+	},
+
+	postToMongo: function(date){
+		
+	}
+};
+
+exports.utils = utils;
