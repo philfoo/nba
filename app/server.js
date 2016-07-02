b8ec7b0646d93883eaa07a98a6ae0e1a7dcd1740
Phil <philphoo95@gmail.com>
2016-07-02 11:03:50 -0700

    basic server setup and gitignore

---
+++ app/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log("Server started!");
+
+/*Eventual index.html file goes here*/
+app.get('/', function(req, res){
+	res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+	console.log("Home page visited");
+});
+
