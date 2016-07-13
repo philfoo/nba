ec535f2baa334aa4d94f2782b8d75bc8dd692b32
Phil <philphoo95@gmail.com>
2016-07-13 00:17:40 -0700

    almost there with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,49 @@
 var gulp = require('gulp');
-var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var reactify = require('reactify'); 
-var concat = require('gulp-concat');
- 
-gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ['./clients/components/main.js'], // Only need initial file, browserify finds the deps
-        transform: [reactify], // We want to convert JSX to normal javascript
-        debug: true, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-    });
-    var watcher  = watchify(bundler);
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 
-    return watcher
-    .on('update', function () { // When any files update
-        var updateStart = Date.now();
-        console.log('Updating!');
-        watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('./clients/components/main.js'))
-    // This is where you add uglifying etc.
-        .pipe(gulp.dest('./client/'));
-        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./build/'));
+var path = {
+    HTML: 'client/index.html',
+    MINIFIED_OUT: 'build.min.js',
+    OUT: 'build.js',
+    DEST: 'dist',
+    DEST_BUILD: './dist/build',
+    DEST_SRC: './dist/src',
+    ENTRY_POINT: './client/components/main.js'
+};
+
+gulp.task('copy', function(){
+    gulp.src(path.HTML)
+        .pipe(gulp.dest(path.DEST));
 });
 
-// I added this so that you see how to run two watch tasks
-// gulp.task('css', function () {
-//     gulp.watch('styles/**/*.css', function () {
-//         return gulp.src('styles/**/*.css')
-//         .pipe(concat('main.css'))
-//         .pipe(gulp.dest('build/'));
-//     });
-// });
+gulp.task('watch', function(){
+    gulp.watch(path.HTML, ['copy']);
 
-// Just running the two tasks
-gulp.task('default', ['browserify']);
+    var watcher = watchify(browserify({
+        entries: [path.ENTRY_POINT],
+        transform: [reactify],
+        debug: true,
+        cache: {}, packageCache: {}, fullPaths: true
+    }));
+
+    return watcher.on('update', function(){
+        watcher.bundle()
+            .pipe(source(path.OUT))
+            .pipe(gulp.dest(path.DEST_SRC))
+            console.log('Updated');
+    })
+        .bundle()
+        .pipe(source(path.OUT))
+        .pipe(gulp.dest(path.DEST_SRC));
+});
+
+gulp.task('default', ['watch']);
+
+
+
